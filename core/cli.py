b897b99d8b61845a5fb3ed6781568aaefb91e73c
Nikita <nrusanovskii923@gmail.com>
2025-08-23 13:59:01 +0300

    Separated responsibilities in the Console class

--- core/cli.py
+++ core/cli.py
@@ -13,6 +13,32 @@ logging.basicConfig(
 logger = logging.getLogger("CLI-logger")
 
 
+# utils
+def convert_args(value: str,
+                 expected_type: int | str) -> int | str:
+    try:
+        if expected_type is int:
+            return int(value)
+        return value
+    except Exception as ex:
+        logger.error(f'Error {ex}.')
+
+
+def parse_args(function: Callable, raw_args: list[str]) -> list[Any]:
+    sign = inspect.signature(function)
+    annotations = list(sign.parameters.values())
+    args = []
+    ann_id = 0
+    for param in raw_args:
+        args.append(convert_args(param,
+                                 annotations[ann_id].annotation))
+        ann_id += 1
+    logger.info((f'Arguments for {function.__name__} has been parsed.\n'
+                 f'Args: {args}'))
+    return args
+
+
+
 class IConsole(ABC):
     @abstractmethod
     def on_command(self):
@@ -22,28 +48,6 @@ class IConsole(ABC):
 class Console(IConsole):
     def __init__(self):
         self.commands = {}
-
-    def convert_args(self, value: str,
-                     expected_type: int | str) -> int | str:
-        try:
-            if expected_type is int:
-                return int(value)
-            return value
-        except Exception as ex:
-            logger.error(f'Error {ex}.')
-
-    def parse_args(self, function: Callable, raw_args: list[str]) -> list[Any]:
-        sign = inspect.signature(function)
-        annotations = list(sign.parameters.values())
-        args = []
-        ann_id = 0
-        for param in raw_args:
-            args.append(self.convert_args(param,
-                                          annotations[ann_id].annotation))
-            ann_id += 1
-        logger.info((f'Arguments for {function.__name__} has been parsed.\n'
-                     f'Args: {args}'))
-        return args
 
     async def on_command(self):
         while True:
@@ -61,7 +65,7 @@ class Console(IConsole):
                 func()
                 continue
             str_args = split_com[1:]
-            args = self.parse_args(func, str_args)
+            args = parse_args(func, str_args)
             try:
                 func(*args)
             except Exception as ex:
